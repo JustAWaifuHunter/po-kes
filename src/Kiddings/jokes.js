4651da3fa5a3b501440cf99a82aaaf68f21f7637
Sayran Felix <88841039+SayranFelix@users.noreply.github.com>
2021-10-13 15:27:55 -0300

    update jokes.js

--- src/Kiddings/jokes.js
+++ src/Kiddings/jokes.js
@@ -1,6 +1,10 @@
 module.exports = {
   jokes: {
-    "pt": ["Você conhece a piada do pônei? Pô nei eu...", "B"],
+    // Portuguese Lang - Part 1.
+    "pt": ["Você conhece a piada do pônei? Pô nei eu...",
+    "Qual o animal que se dissolve na água? O orangoTANG."],
+    
+    // English Lang - Part 2.
     "en": ["C", "D"]
   }
 }
