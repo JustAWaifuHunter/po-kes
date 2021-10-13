f78b054355ff64605ceb2aafeb5e37a3d8725d0b
Sayran Felix <88841039+SayranFelix@users.noreply.github.com>
2021-10-13 08:27:16 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
+/* Jokes - Part 1 of Index. */
 const JokesJS = require("./src/Kiddings/jokes.js")
 
-class Jokes {
-  constructor() {
-    let rand = Math.floor(Math.random() * JokesJS.jokes.length)
-    return Jokes.jokes[rand]
-  }
+function Jokes() {
+  let rand = Math.floor(Math.random() * JokesJS.jokes.length)
+    return JokesJS.jokes[rand]
 }
 
-let ha = new Jokes()
+let ha = Jokes()
 console.log(ha)
