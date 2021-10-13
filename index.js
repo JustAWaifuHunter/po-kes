d81fe2f7d7e38789daf4d5befab6ba8511654997
Sayran Felix <88841039+SayranFelix@users.noreply.github.com>
2021-10-13 08:34:09 -0300

    Delete the file at 'index.js'

--- index.js
+++
@@ -1,7 +0,0 @@
-/* Jokes - Part 1 of Index. */
-const JokesJS = require("./src/Kiddings/jokes.js");
-
-function Jokes() {
-  let rand = Math.floor(Math.random() * JokesJS.jokes.length)
-    return JokesJS.jokes[rand]
-}
