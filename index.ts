157ea0e222fc44d027965ca149b2587257b07666
Sayran Felix <88841039+SayranFelix@users.noreply.github.com>
2021-10-13 09:57:05 -0300

    update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const JokesJS = require("./src/Kiddings/jokes.js");
 
 function Jokes(x: number) {
   if (x) {
-    return JokesJS[x]
+    return JokesJS.jokes[x]
   } else {
    let rand = Math.floor(Math.random() * JokesJS.jokes.length)
     return JokesJS.jokes[rand]
