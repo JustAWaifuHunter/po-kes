299988f57a7b6b47776de0002255872a46d114f7
Sayran Felix <88841039+SayranFelix@users.noreply.github.com>
2021-10-13 12:12:26 -0300

    update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 /* Jokes - Part 1 of Index. */
 const JokesJS = require("./src/Kiddings/jokes.js");
 
-function Jokes(x: number) {
+function Joke(lang: string, x: number) {
+  /* Language Select or Default: Portuguese. */
+  let language;
+  if (!lang || lang == 'pt') {
+    language = "pt"
+  } else if (lang == 'en') {
+    language = "en"
+  }
+  
+  /* Jokes Select or Random. */
   if (x) {
-    return JokesJS.jokes[x]
+    return JokesJS.jokes[language][x]
   } else {
-   let rand = Math.floor(Math.random() * JokesJS.jokes.length)
-    return JokesJS.jokes[rand]
+   let rand = Math.floor(Math.random() * JokesJS.jokes["pt"].length)
+    return JokesJS.jokes["pt"][rand]
   }
 }
