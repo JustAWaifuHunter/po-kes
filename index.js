0395966a22d115742b161f87f271d6c5302d01a3
Sayran Felix <88841039+SayranFelix@users.noreply.github.com>
2021-10-13 08:14:15 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const JokesJS = require("./src/JokesJS/jokes.js")
 
+class Jokes {
+  constructor() {
+    let rand = Math.floor(Math.random() * JokesJS.jokes.length)
+    return Jokes[rand]
+  }
+}
+
+let ha = new Jokes()
+console.log(ha)
